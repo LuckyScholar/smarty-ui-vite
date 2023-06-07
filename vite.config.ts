cd89cf147f321c6896bb554b5e9ce0c80b690a68
LuckyScholar <lucky837416656@gmail.com>
2023-06-07 16:17:55 +0800

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import Unocss from "./config/unocss";
+
+// https://vitejs.dev/config/
+const rollupOptions = {
+  external: ["vue", "vue-router"],
+  output: {
+    globals: {
+      vue: "Vue",
+    },
+  },
+};
+
+export default defineConfig({
+  plugins: [vue(), vueJsx(), Unocss()],
+
+  // 添加库模式配置
+  build: {
+    rollupOptions,
+    minify: false,
+    cssCodeSplit: true, // 独立输出 css
+    lib: {
+      entry: "./src/entry.ts",
+      name: "SmartyUI",
+      fileName: "smarty-ui",
+      // 导出模块格式
+      formats: ["esm", "umd", "iife"],
+    },
+  },
+});
